50a60ac9df6f04c2666cb42d38a0335591809578
clbrmm <clbrmmweb@gmail.com>
2023-12-19 12:39:58 -0500

    update utils mock

---
+++ src/utils/helper_functions.py
@@ -0,0 +1,13 @@
+"""
+constants.py - Module for Storing Constants in Automation
+
+This module contains constants used throughout the automation project.
+
+Author: [Your Name]
+"""
+
+# Example constants, replace with actual values as needed.
+PROCESS_A_THRESHOLD = 10
+API_BASE_URL = "https://api.example.com"
+LOG_FILE_PATH = "/path/to/log/file.log"
+# Add more constants as needed for your project.
